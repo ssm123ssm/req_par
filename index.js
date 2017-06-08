17dc02e83e14f6fd71930720c68317272ffcef1f
ssm123ssm <ssm123ssm@gmail.com>
2017-06-08 07:33:22 +0530

    fix

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 var express = require('express');
 var app = express();
+var exip = require('externalip');
 
 app.get("/api", function(req, res){
       
-    var ip = req.ip;
     var lan_full = req.headers["accept-language"];
     var lan = lan_full.substr(0,lan_full.indexOf(","));
-    var os = getOs(process.platform.toString());
-    var ret = {ip:null, language:null, OS:null, User_agent:null};
-    
-    ret.ip = ip;
+    var os;
+    var ret = {ip:null, language:null, OS:null};
     ret.language = lan;
-    ret.OS = os;
-    ret.User_agent = req.headers['user-agent'];
-    res.send(ret);
-    res.end();
+    //ret.OS = os;
+    
+    var ua = req.headers['user-agent'];
+    var ua1 = ua.substr(ua.indexOf("(") + 1);
+    var ua2 = ua1.substr(0,ua1.indexOf(")"));
+   // console.log(ua2);
+    ret.OS=ua2;
+    
+    //ret.User_agent = req.headers['user-agent'];
+    exip(function(err, ip){
+        ret.ip = ip;
+        res.send(ret);
+    
+    });
+    
+    
+    
 });
 app.use(express.static("public"));
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 80);
 
 
 
